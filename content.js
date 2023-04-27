e2c650d4a387d748e7b1b1159da2e50117360d0a
2morrowman <2morrowman@gmail.com>
2023-04-27 22:24:33 +0300

    ChatGPT UX Plus V1.0
    Chat max width and attempt to set Roboto Mono for a code

---
+++ content.js
@@ -0,0 +1,6 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    /*if (request.action === 'adjustStyles') {
+        // Add your custom style adjustments here
+        document.body.style.backgroundColor = 'lightblue';
+    }*/
+});
